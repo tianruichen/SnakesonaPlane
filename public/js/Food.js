8a4c646f31257fc9727c7c3787c90e7a82cd5cd3
Kelpflakes <Kelpflakes@users.noreply.github.com>
2015-10-03 19:02:08 -0700

    Merge remote-tracking branch 'origin/snake' into snake
    Conflicts:
    	server.js

---
+++ public/js/Food.js
@@ -0,0 +1,29 @@
+var Food = function(startX, startY) {
+	var x = startX,
+		y = startY,
+        id
+    
+    var getX = function() {
+        return x;
+    };
+
+    var getY = function() {
+        return y;
+    };
+
+	var draw = function(ctx) {
+        context.beginPath();
+        context.arc(x, y, radius, 0, 2 * Math.PI, false);
+        context.fillStyle = 'green';
+        context.fill();
+	};
+
+	return {
+        getX: getX,
+        getY: getY,
+        setX: setX,
+        setY: setY,
+		update: update,
+		draw: draw
+	}
+};
